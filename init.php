b73b93e8f5c19525a3cf43ad43481ff0cd3051d5
Eugenio Lopez <eugenionlopez@gmail.com>
2018-07-11 09:00:42 -0400

    Change the way init.php requires files
    - Change all require() statements on init.php to
    require_once(), since files should not be
    included multiple times
    - Add require_once() for Uuid.php

--- init.php
+++ init.php
@@ -1,20 +1,21 @@
 <?php
 
 // OnePay Singleton
-require(dirname(__FILE__) . '/lib/OnePay.php');
+require_once(dirname(__FILE__) . '/lib/OnePay.php');
 
 // Utilities
-require(dirname(__FILE__) . '/lib/utils/HttpClient.php');
-require(dirname(__FILE__) . '/lib/utils/OnePayRequestBuilder.php');
-require(dirname(__FILE__) . '/lib/utils/OnePaySignUtil.php');
+require_once(dirname(__FILE__) . '/lib/utils/HttpClient.php');
+require_once(dirname(__FILE__) . '/lib/utils/OnePayRequestBuilder.php');
+require_once(dirname(__FILE__) . '/lib/utils/OnePaySignUtil.php');
+require_once(dirname(__FILE__) . '/lib/utils/Uuid.php');
 
 // Constants
-require(dirname(__FILE__) . '/lib/constants/Constants.php');
+require_once(dirname(__FILE__) . '/lib/constants/Constants.php');
 
 // Model classes
-require(dirname(__FILE__) . '/lib/BaseRequest.php');
-require(dirname(__FILE__) . '/lib/Item.php');
-require(dirname(__FILE__) . '/lib/Options.php');
-require(dirname(__FILE__) . '/lib/ShoppingCart.php');
-require(dirname(__FILE__) . '/lib/Transaction.php');
-require(dirname(__FILE__) . '/lib/TransactionCreateRequest.php');
+require_once(dirname(__FILE__) . '/lib/BaseRequest.php');
+require_once(dirname(__FILE__) . '/lib/Item.php');
+require_once(dirname(__FILE__) . '/lib/Options.php');
+require_once(dirname(__FILE__) . '/lib/ShoppingCart.php');
+require_once(dirname(__FILE__) . '/lib/Transaction.php');
+require_once(dirname(__FILE__) . '/lib/TransactionCreateRequest.php');
