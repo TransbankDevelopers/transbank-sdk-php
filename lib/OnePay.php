90f1ce228aaf268e1ed92565cd124656b71422df
Eugenio Lopez <eugenionlopez@gmail.com>
2018-07-18 12:33:42 -0400

    Remove code that was commented out

--- lib/OnePay.php
+++ lib/OnePay.php
@@ -21,10 +21,6 @@ class OnePay
     public static $scriptPath;
     public static $apiKey;
     public static $sharedSecret;
-    // const INTEGRATION_TYPES = array("TEST" => 'https://web2desa.test.transbank.cl',
-    //                                 "LIVE" => '',
-    //                                 "MOCK" => 'http://onepay.getsandbox.com');
-
     private static $integrationType = "MOCK";
     /**
      * Return the API key used for requests
