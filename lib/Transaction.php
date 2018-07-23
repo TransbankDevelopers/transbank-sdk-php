bf099fcaa87d6c0be7ed139193b982e1f2f13c40
Eugenio Lopez <eugenionlopez@gmail.com>
2018-07-22 21:19:30 -0400

    WIP: Add response & signature validation to transaction

--- lib/Transaction.php
+++ lib/Transaction.php
@@ -23,12 +23,28 @@ namespace Transbank;
             throw new \Exception("Shopping cart is null or empty");
         }
         $http = new HttpClient();
-
+        $options = OnePayRequestBuilder::getInstance()->buildOptions($options);
         $request = json_encode(OnePayRequestBuilder::getInstance()->buildCreateRequest($shoppingCart, $options), JSON_UNESCAPED_SLASHES);
         
         $path = self::TRANSACTION_BASE_PATH . self::SEND_TRANSACTION;
-        $httpResponse = $http->post(OnePay::getCurrentIntegrationTypeUrl(), $path ,$request);
-        return (new TransactionCreateResponse())->fromJSON($httpResponse);
+        $httpResponse = json_decode($http->post(OnePay::getCurrentIntegrationTypeUrl(), $path ,$request), true);
+
+        if (!$httpResponse) {
+            throw new TransactionCreateException(-1, 'Could not obtain a response from the service');
+        }
+        if($httpResponse['responseCode'] != "OK") {
+            throw new TransactionCreateException(-1, $httpResponse['responseCode'] . " : " . $httpResponse['description']);
+        }
+
+        $transactionCreateResponse =  (new TransactionCreateResponse())
+                                      ->fromJSON($httpResponse);
+        $signatureIsValid = OnePaySignUtil::getInstance()
+                            ->validate($transactionCreateResponse,
+                                        $options->getSharedSecret());
+        if (!$signatureIsValid) {
+            throw new SignatureException(-1, "The response signature is not valid");
+        }
+        return $transactionCreateResponse;
     }
 
     public static function commit($occ, $externalUniqueNumber, $options)
