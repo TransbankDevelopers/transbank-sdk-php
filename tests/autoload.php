656273f673fa513ef3205bebef3fd086167d18c6
DarkGhostHunter <DarkGhostHunter@Gmail.com>
2018-11-02 18:07:42 -0300

    Restored the Autoload for Tests Changed position of the SDK COPY operation on the Docker image.

--- tests/autoload.php
+++ tests/autoload.php
@@ -1,18 +1,15 @@
 <?php
 
-//include_once __DIR__.'/../vendor/autoload.php';
-//
-//$classLoader = new \Composer\Autoload\ClassLoader();
-//
-//$classLoader->addPsr4("Transbank\\Onepay\\", __DIR__ .'/../src/Onepay', true);
-//$classLoader->addPsr4("Transbank\\Onepay\\", __DIR__ .'/../src/Onepay/utils', true);
-//$classLoader->addPsr4("Transbank\\Onepay\\Exceptions\\", __DIR__ .'/../src/Onepay/exceptions', true);
-//
-//$classLoader->addPsr4("Transbank\\Webpay\\", __DIR__ .'/../src/Webpay', true);
-//
-//$classLoader->addPsr4("Transbank\\Helpers\\", __DIR__ .'/../src/Helpers', true);
-//
-//$classLoader->register();
+include_once __DIR__.'/../vendor/autoload.php';
 
-// Let's use this so Sophomore doesn't cry
-require_once(__DIR__ . '/../init.php');
+$classLoader = new \Composer\Autoload\ClassLoader();
+
+$classLoader->addPsr4("Transbank\\Onepay\\", __DIR__ .'/../src/Onepay', true);
+$classLoader->addPsr4("Transbank\\Onepay\\", __DIR__ .'/../src/Onepay/utils', true);
+$classLoader->addPsr4("Transbank\\Onepay\\Exceptions\\", __DIR__ .'/../src/Onepay/exceptions', true);
+
+$classLoader->addPsr4("Transbank\\Webpay\\", __DIR__ .'/../src/Webpay', true);
+
+$classLoader->addPsr4("Transbank\\Helpers\\", __DIR__ .'/../src/Helpers', true);
+
+$classLoader->register();
