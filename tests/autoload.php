0aef370323e246b8b7717630b72768cfa4dddb49
DarkGhostHunter <DarkGhostHunter@Gmail.com>
2018-11-02 17:52:49 -0300

    Changed to a bootstraped file for tests

--- tests/autoload.php
+++ tests/autoload.php
@@ -1,15 +1,18 @@
 <?php
 
-include_once __DIR__.'/../vendor/autoload.php';
+//include_once __DIR__.'/../vendor/autoload.php';
+//
+//$classLoader = new \Composer\Autoload\ClassLoader();
+//
+//$classLoader->addPsr4("Transbank\\Onepay\\", __DIR__ .'/../src/Onepay', true);
+//$classLoader->addPsr4("Transbank\\Onepay\\", __DIR__ .'/../src/Onepay/utils', true);
+//$classLoader->addPsr4("Transbank\\Onepay\\Exceptions\\", __DIR__ .'/../src/Onepay/exceptions', true);
+//
+//$classLoader->addPsr4("Transbank\\Webpay\\", __DIR__ .'/../src/Webpay', true);
+//
+//$classLoader->addPsr4("Transbank\\Helpers\\", __DIR__ .'/../src/Helpers', true);
+//
+//$classLoader->register();
 
-$classLoader = new \Composer\Autoload\ClassLoader();
-
-$classLoader->addPsr4("Transbank\\Onepay\\", __DIR__ .'/../src/Onepay', true);
-$classLoader->addPsr4("Transbank\\Onepay\\", __DIR__ .'/../src/Onepay/utils', true);
-$classLoader->addPsr4("Transbank\\Onepay\\Exceptions\\", __DIR__ .'/../src/Onepay/exceptions', true);
-
-$classLoader->addPsr4("Transbank\\Webpay\\", __DIR__ .'/../src/Webpay', true);
-
-$classLoader->addPsr4("Transbank\\Helpers\\", __DIR__ .'/../src/Helpers', true);
-
-$classLoader->register();
+// Let's use this so Sophomore doesn't cry
+require_once(__DIR__ . '/../init.php');
