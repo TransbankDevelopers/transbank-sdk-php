0b9c01f624e480a6d755f86956761e12c6d069a8
DarkGhostHunter <DarkGhostHunter@Gmail.com>
2018-11-02 17:39:27 -0300

    Fixed Transbank\Webpay namespace

--- tests/autoload.php
+++ tests/autoload.php
@@ -9,7 +9,6 @@ $classLoader->addPsr4("Transbank\\Onepay\\", __DIR__ .'/../src/Onepay/utils', tr
 $classLoader->addPsr4("Transbank\\Onepay\\Exceptions\\", __DIR__ .'/../src/Onepay/exceptions', true);
 
 $classLoader->addPsr4("Transbank\\Webpay\\", __DIR__ .'/../src/Webpay', true);
-$classLoader->addPsr4("Transbank\\Webpay\\", __DIR__ .'/../src/Webpay/Soap', true);
 
 $classLoader->addPsr4("Transbank\\Helpers\\", __DIR__ .'/../src/Helpers', true);
 
