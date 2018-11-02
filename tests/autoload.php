f11a46f548acf0a33ae242bb0d0ae34695b815f1
DarkGhostHunter <DarkGhostHunter@Gmail.com>
2018-11-02 17:38:49 -0300

    Fixed Soap directory (was lowercase)

--- tests/autoload.php
+++ tests/autoload.php
@@ -3,11 +3,14 @@
 include_once __DIR__.'/../vendor/autoload.php';
 
 $classLoader = new \Composer\Autoload\ClassLoader();
+
 $classLoader->addPsr4("Transbank\\Onepay\\", __DIR__ .'/../src/Onepay', true);
 $classLoader->addPsr4("Transbank\\Onepay\\", __DIR__ .'/../src/Onepay/utils', true);
 $classLoader->addPsr4("Transbank\\Onepay\\Exceptions\\", __DIR__ .'/../src/Onepay/exceptions', true);
+
 $classLoader->addPsr4("Transbank\\Webpay\\", __DIR__ .'/../src/Webpay', true);
-$classLoader->addPsr4("Transbank\\Webpay\\", __DIR__ .'/../src/Webpay/soap', true);
+$classLoader->addPsr4("Transbank\\Webpay\\", __DIR__ .'/../src/Webpay/Soap', true);
+
 $classLoader->addPsr4("Transbank\\Helpers\\", __DIR__ .'/../src/Helpers', true);
 
 $classLoader->register();
