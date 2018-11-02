df0a134de8e54ab437c58c6d8f608fc12c2320c4
DarkGhostHunter <DarkGhostHunter@Gmail.com>
2018-11-02 16:50:43 -0300

    Added back the init.php for non-composer user.

---
+++ init.php
@@ -0,0 +1,64 @@
+<?php
+
+// Onepay Singleton
+require_once(dirname(__FILE__) . '/src/Onepay/ChannelEnum.php');
+require_once(dirname(__FILE__) . '/src/Onepay/OnepayBase.php');
+
+// Utilities
+require_once(dirname(__FILE__) . '/src/Onepay/utils/HttpClient.php');
+require_once(dirname(__FILE__) . '/src/Onepay/utils/OnepayRequestBuilder.php');
+require_once(dirname(__FILE__) . '/src/Onepay/utils/OnepaySignUtil.php');
+
+// Model classes
+require_once(dirname(__FILE__) . '/src/Onepay/BaseRequest.php');
+require_once(dirname(__FILE__) . '/src/Onepay/BaseResponse.php');
+require_once(dirname(__FILE__) . '/src/Onepay/Item.php');
+require_once(dirname(__FILE__) . '/src/Onepay/Options.php');
+require_once(dirname(__FILE__) . '/src/Onepay/ShoppingCart.php');
+require_once(dirname(__FILE__) . '/src/Onepay/Transaction.php');
+
+require_once(dirname(__FILE__) . '/src/Onepay/TransactionCreateRequest.php');
+require_once(dirname(__FILE__) . '/src/Onepay/TransactionCreateResponse.php');
+
+require_once(dirname(__FILE__) . '/src/Onepay/TransactionCommitRequest.php');
+require_once(dirname(__FILE__) . '/src/Onepay/TransactionCommitResponse.php');
+
+require_once(dirname(__FILE__) . '/src/Onepay/Refund.php');
+require_once(dirname(__FILE__) . '/src/Onepay/RefundCreateRequest.php');
+require_once(dirname(__FILE__) . '/src/Onepay/RefundCreateResponse.php');
+
+// Exceptions
+require_once(dirname(__FILE__) . '/src/Onepay/exceptions/TransbankException.php');
+require_once(dirname(__FILE__) . '/src/Onepay/exceptions/AmountException.php');
+require_once(dirname(__FILE__) . '/src/Onepay/exceptions/RefundCreateException.php');
+require_once(dirname(__FILE__) . '/src/Onepay/exceptions/SignException.php');
+require_once(dirname(__FILE__) . '/src/Onepay/exceptions/TransactionCommitException.php');
+require_once(dirname(__FILE__) . '/src/Onepay/exceptions/TransactionCreateException.php');
+
+// WEBPAY
+require_once(dirname(__FILE__) . '/src/Webpay/Configuration.php');
+require_once(dirname(__FILE__) . '/src/Webpay/IntegrationConfiguration.php');
+require_once(dirname(__FILE__) . '/src/Webpay/Webpay.php');
+
+require_once(dirname(__FILE__) . '/src/Webpay/ClassMaps/classmaps.php');
+require_once(dirname(__FILE__) . '/src/Webpay/ResultCodes/oneclicknormal.php');
+require_once(dirname(__FILE__) . '/src/Webpay/ResultCodes/pluscapture.php');
+require_once(dirname(__FILE__) . '/src/Webpay/ResultCodes/plusnormal.php');
+
+require_once(dirname(__FILE__) . '/src/Webpay/Soap/SoapValidation.php');
+require_once(dirname(__FILE__) . '/src/Webpay/Soap/WSSecuritySoapClient.php');
+require_once(dirname(__FILE__) . '/src/Webpay/Soap/WSSESoap.php');
+require_once(dirname(__FILE__) . '/src/Webpay/Soap/XMLSecEnc.php');
+require_once(dirname(__FILE__) . '/src/Webpay/Soap/XMLSecurityDSig.php');
+require_once(dirname(__FILE__) . '/src/Webpay/Soap/XMLSecurityKey.php');
+
+require_once(dirname(__FILE__) . '/src/Webpay/Transactions/Concerns/AcknowledgesTransactions.php');
+require_once(dirname(__FILE__) . '/src/Webpay/Transactions/Concerns/InitializesTransactions.php');
+require_once(dirname(__FILE__) . '/src/Webpay/Transactions/Concerns/PerformsGetTransactionResults.php');
+require_once(dirname(__FILE__) . '/src/Webpay/Transactions/Transaction.php');
+require_once(dirname(__FILE__) . '/src/Webpay/Transactions/WebpayCapture.php');
+require_once(dirname(__FILE__) . '/src/Webpay/Transactions/WebpayCompleteTransaction.php');
+require_once(dirname(__FILE__) . '/src/Webpay/Transactions/WebpayMallNormal.php');
+require_once(dirname(__FILE__) . '/src/Webpay/Transactions/WebpayNormal.php');
+require_once(dirname(__FILE__) . '/src/Webpay/Transactions/WebpayNullify.php');
+require_once(dirname(__FILE__) . '/src/Webpay/Transactions/WebpayOneclick.php');
