cca2a9c7c81dcbc6e7042b5f43283c93aceaa5fa
Eugenio Nicolás López Ramos <eugenionlopez@gmail.com>
2019-06-27 10:18:57 -0400

    Basic Refund impl

--- lib/webpay_rest/webpay_plus/Transaction.php
+++ lib/webpay_rest/webpay_plus/Transaction.php
@@ -123,11 +123,49 @@ class Transaction
         return $transactionCommitResponse;
     }
 
-    public static function refund($token, $amount, $options)
+    public static function refund($token, $amount, $options = null)
     {
+        if ($options == null) {
+            $commerceCode = WebpayPlus::getCommerceCode();
+            $apiKey = WebpayPlus::getApiKey();
+            $baseUrl = WebpayPlus::getIntegrationTypeUrl();
+        } else {
+            $commerceCode = $options->getCommerceCode();
+            $apiKey = $options->getApiKey();
+            $baseUrl = WebpayPlus::getIntegrationTypeUrl($options->getIntegrationType());
+        }
+
+        $headers = [
+            "Tbk-Api-Key-Id" => $commerceCode,
+            "Tbk-Api-Key-Secret" => $apiKey
+        ];
+
+        $payload = [
+            "token" => $token,
+            "amount" => $amount
+        ];
 
         $url = str_replace('$TOKEN$', $token, self::REFUND_TRANSACTION_ENDPOINT);
 
+        $http = WebpayPlus::getHttpClient();
+        $httpResponse = $http->post($baseUrl,
+            $url,
+            $payload,
+            ['headers' => $headers]
+        );
 
+        if (!$httpResponse) {
+            throw new TransactionCommitException('Could not obtain a response from the service', -1);
+        }
+
+        $responseJson = json_decode($httpResponse, true);
+
+        if (array_key_exists("error_message", $responseJson)) {
+            throw new TransactionCommitException($responseJson['error_message']);
+        }
+
+        $transactionRefundResponse = new TransactionRefundResponse($responseJson);
+
+        return $transactionRefundResponse;
     }
 }
