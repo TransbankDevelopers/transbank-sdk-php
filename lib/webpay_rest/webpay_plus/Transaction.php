2e7bef3cfb3136f7957f2ad7c1d6e30ae459958f
Eugenio Nicolás López Ramos <eugenionlopez@gmail.com>
2019-07-02 11:54:42 -0400

    Remove mall
    - Remove the Mall\Transaction class because it was originally thought that it'd
    extend the WebpayPlus Transaction class and then override the methods
    that are different on Mall, however this is not possible because the
    method signatures are different, and this is not allowed on PHP

--- lib/webpay_rest/webpay_plus/Transaction.php
+++ lib/webpay_rest/webpay_plus/Transaction.php
@@ -7,6 +7,7 @@ use Transbank\Webpay\Exceptions\TransactionCreateException;
 use Transbank\Webpay\Exceptions\TransactionStatusException;
 use Transbank\Webpay\Options;
 use Transbank\Webpay\WebpayPlus;
+use Transbank\Webpay\WebpayPlus\Mall\TransactionRefundException;
 
 class Transaction
 {
@@ -211,5 +212,107 @@ class Transaction
         return $transactionStatusResponse;
     }
 
+    public static function refundMall($token, $buyOrder, $commerceCode, $amount, $options = null)
+    {
+        if ($options == null) {
+            $commerceCode = WebpayPlus::getCommerceCode();
+            $apiKey = WebpayPlus::getApiKey();
+            $baseUrl = WebpayPlus::getIntegrationTypeUrl();
+        } else {
+            $commerceCode = $options->getCommerceCode();
+            $apiKey = $options->getApiKey();
+            $baseUrl = WebpayPlus::getIntegrationTypeUrl($options->getIntegrationType());
+        }
+
+        $headers = [
+            "Tbk-Api-Key-Id" => $commerceCode,
+            "Tbk-Api-Key-Secret" => $apiKey
+        ];
+
+        $payload = json_encode(
+            [
+                "token" => $token,
+                "buy_order" => $buyOrder,
+                "commerce_code" => $commerceCode
+            ]);
+
+        $http = WebpayPlus::getHttpClient();
+
+        $url = str_replace('$TOKEN$', $token,
+            \Transbank\Webpay\WebpayPlus\Mall\Transaction::REFUND_TRANSACTION_ENDPOINT);
+
+        $httpResponse = $http->post($baseUrl,
+            $url,
+            $payload,
+            ['headers' => $headers]
+        );
+
+        if (!$httpResponse) {
+            throw new TransactionRefundException('Could not obtain a response from the service', -1);
+        }
+
+        $responseJson = json_decode($httpResponse, true);
+
+        if (array_key_exists("error_message", $responseJson)) {
+            throw new TransactionRefundException($responseJson['error_message']);
+        }
+
+        $transactionRefundResponse = new TransactionRefundResponse($responseJson);
+
+        return $transactionRefundResponse;
+    }
+
+    public static function createMall(
+        $buyOrder,
+        $sessionId,
+        $returnUrl,
+        $details,
+        $options = null
+    ) {
+        if ($options == null) {
+            $commerceCode = WebpayPlus::getCommerceCode();
+            $apiKey = WebpayPlus::getApiKey();
+            $baseUrl = WebpayPlus::getIntegrationTypeUrl();
+        } else {
+            $commerceCode = $options->getCommerceCode();
+            $apiKey = $options->getApiKey();
+            $baseUrl = WebpayPlus::getIntegrationTypeUrl($options->getIntegrationType());
+        }
+
+        $headers = [
+            "Tbk-Api-Key-Id" => $commerceCode,
+            "Tbk-Api-Key-Secret" => $apiKey
+        ];
+
+        $payload = json_encode([
+            "buy_order" => $buyOrder,
+            "session_id" => $sessionId,
+            "details" => $details,
+            "return_url" => $returnUrl
+        ]);
+
+        $http = WebpayPlus::getHttpClient();
+
+        $httpResponse = $http->post($baseUrl,
+            \Transbank\Webpay\WebpayPlus\Mall\Transaction::CREATE_TRANSACTION_ENDPOINT,
+            $payload,
+            ['headers' => $headers]
+        );
+
+        if (!$httpResponse) {
+            throw new TransactionCreateException('Could not obtain a response from the service', -1);
+        }
+
+        $responseJson = json_decode($httpResponse, true);
+        if (!$responseJson["token"] || !$responseJson['url']) {
+            throw new TransactionCreateException($responseJson['error_message']);
+        }
+
+        $json = json_decode($httpResponse, true);
+
+        $transactionCreateResponse = new TransactionCreateResponse($json);
+
+        return $transactionCreateResponse;
+    }
 
 }
