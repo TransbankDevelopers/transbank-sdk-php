76271fd1f9b4b11017972139d9e34063a4dfa9bb
Eugenio Nicolás López Ramos <eugenionlopez@gmail.com>
2019-07-10 14:48:59 -0400

    Add Oneclick mall authorize

--- lib/webpay_rest/oneclick/MallTransaction.php
+++ lib/webpay_rest/oneclick/MallTransaction.php
@@ -4,18 +4,62 @@
 namespace Transbank\Webpay\Oneclick;
 
 
+use Transbank\Webpay\Exceptions\AuthorizeMallTransactionException;
+use Transbank\Webpay\Exceptions\InscriptionStartException;
+use Transbank\Webpay\Oneclick;
+
 class MallTransaction
 {
 
-    const AUTHORIZE_TRANSACTION_ENDPOINT = '/rswebpaytransaction/api/oneclick/v1.0/transaction';
-    const TRANSACTION_STATUS_ENDPONT = '/rswebpaytransaction/api/oneclick/v1.0/transactions/$BUYORDER$';
-    const TRANSACTION_REFUND_ENDPOINT = '/rswebpaytransaction/api/oneclick/v1.0/transactions/$BUYORDER$/refund';
+    const AUTHORIZE_TRANSACTION_ENDPOINT = 'rswebpaytransaction/api/oneclick/v1.0/transaction';
+    const TRANSACTION_STATUS_ENDPONT = 'rswebpaytransaction/api/oneclick/v1.0/transactions/$BUYORDER$';
+    const TRANSACTION_REFUND_ENDPOINT = 'rswebpaytransaction/api/oneclick/v1.0/transactions/$BUYORDER$/refund';
 
-    public static function authorize($userName, $tbkUser, $amount, $buyOrder,
+    public static function authorize($userName, $tbkUser, $buyOrder, $details,
                                      $options = null)
     {
 
+        if ($options == null) {
+            $commerceCode = Oneclick::getCommerceCode();
+            $apiKey = Oneclick::getApiKey();
+            $baseUrl = Oneclick::getIntegrationTypeUrl();
+        } else {
+            $commerceCode = $options->getCommerceCode();
+            $apiKey = $options->getApiKey();
+            $baseUrl = WebpayPlus::getIntegrationTypeUrl($options->getIntegrationType());
+        }
 
+        $http = Oneclick::getHttpClient();
+        $headers = [
+            "Tbk-Api-Key-Id" => $commerceCode,
+            "Tbk-Api-Key-Secret" => $apiKey
+        ];
+
+        $payload = json_encode([
+            "username" => $userName,
+            "tbk_user" => $tbkUser,
+            "buy_order" => $buyOrder,
+            "details" => $details]);
+
+        $httpResponse = $http->post($baseUrl,
+            self::AUTHORIZE_TRANSACTION_ENDPOINT,
+            $payload,
+            ['headers' => $headers]
+        );
+
+        if (!$httpResponse) {
+            throw new AuthorizeMallTransactionException('Could not obtain a response from the service', -1);
+        }
+
+        $responseJson = json_decode($httpResponse, true);
+        if (isset($responseJson['error_message'])) {
+            throw new AuthorizeMallTransactionException($responseJson['error_message']);
+        }
+        $json = json_decode($httpResponse, true);
+
+        $authorizeTransactionResponse = new AuthorizeMallTransactionResponse($json);
+
+        return $authorizeTransactionResponse;
     }
 
     public static function getStatus($buyOrder, $options = null)
---
+++ lib/webpay_rest/oneclick/exceptions/AuthorizeMallTransactionResponse.php
@@ -0,0 +1,162 @@
+<?php
+
+
+namespace Transbank\Webpay\Oneclick;
+
+
+class AuthorizeMallTransactionResponse
+{
+    public $buyOrder;
+    public $sessionId;
+    public $cardNumber;
+    public $expirationDate;
+    public $accountingDate;
+    public $transactionDate;
+    public $details;
+
+    public function __construct($json)
+    {
+        $buyOrder = isset($json["buy_order"]) ? $json["buy_order"] : null;
+        $sessionId = isset($json["session_id"]) ? $json["session_id"] : null;
+        $cardNumber = isset($json["card_detail"]) ? isset($json["card_detail"]["card_number"]) ? $json["card_detail"]["card_number"] : null : null;
+
+        $expirationDate = isset($json["expiration_date"]) ? $json["expiration_date"] : null;
+        $accountingDate = isset($json["accounting:date"]) ? $json["accounting:date"] : null;
+        $transactionDate = isset($json["transaction_date"]) ? $json["transaction_date"] : null;
+        $details = isset($json["details"]) ? $json["details"] : null;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBuyOrder()
+    {
+        return $this->buyOrder;
+    }
+
+    /**
+     * @param mixed $buyOrder
+     *
+     * @return AuthorizeMallTransactionResponse
+     */
+    public function setBuyOrder($buyOrder)
+    {
+        $this->buyOrder = $buyOrder;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSessionId()
+    {
+        return $this->sessionId;
+    }
+
+    /**
+     * @param mixed $sessionId
+     *
+     * @return AuthorizeMallTransactionResponse
+     */
+    public function setSessionId($sessionId)
+    {
+        $this->sessionId = $sessionId;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCardNumber()
+    {
+        return $this->cardNumber;
+    }
+
+    /**
+     * @param mixed $cardNumber
+     *
+     * @return AuthorizeMallTransactionResponse
+     */
+    public function setCardNumber($cardNumber)
+    {
+        $this->cardNumber = $cardNumber;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getExpirationDate()
+    {
+        return $this->expirationDate;
+    }
+
+    /**
+     * @param mixed $expirationDate
+     *
+     * @return AuthorizeMallTransactionResponse
+     */
+    public function setExpirationDate($expirationDate)
+    {
+        $this->expirationDate = $expirationDate;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAccountingDate()
+    {
+        return $this->accountingDate;
+    }
+
+    /**
+     * @param mixed $accountingDate
+     *
+     * @return AuthorizeMallTransactionResponse
+     */
+    public function setAccountingDate($accountingDate)
+    {
+        $this->accountingDate = $accountingDate;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTransactionDate()
+    {
+        return $this->transactionDate;
+    }
+
+    /**
+     * @param mixed $transactionDate
+     *
+     * @return AuthorizeMallTransactionResponse
+     */
+    public function setTransactionDate($transactionDate)
+    {
+        $this->transactionDate = $transactionDate;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDetails()
+    {
+        return $this->details;
+    }
+
+    /**
+     * @param mixed $details
+     *
+     * @return AuthorizeMallTransactionResponse
+     */
+    public function setDetails($details)
+    {
+        $this->details = $details;
+        return $this;
+    }
+
+}
