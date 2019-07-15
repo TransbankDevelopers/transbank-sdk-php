b498d03491bfebc14232a897ab37fe0b150cde61
Eugenio Nicolás López Ramos <eugenionlopez@gmail.com>
2019-07-15 12:22:19 -0400

    Testing

--- lib/utils/HttpClient.php
+++ lib/utils/HttpClient.php
@@ -1,42 +1,46 @@
 <?php
 namespace Transbank\Utils;
 
+use GuzzleHttp\Psr7\Request;
+
 class HttpClient {
 
     function post($url, $path, $data_to_send, $options = array('headers' => 0, 'transport' => 'https', 'port' => 443, 'proxy' => null)) {
 
 
-        $curl = curl_init();
-        $optionsHeaders = $options["headers"] ? $options["headers"] : [];
+        $req = new Request('POST', $url . $path, $options, $data_to_send);
 
-        $basicHeader = ["Content-Type" => "application/json"];
-        $headers = $this->toHeaderStrings(
-            array_merge($basicHeader, $optionsHeaders)
-        );
+//        $curl = curl_init();
+//        $optionsHeaders = $options["headers"] ? $options["headers"] : [];
+//
+//        $basicHeader = ["Content-Type" => "application/json"];
+//        $headers = $this->toHeaderStrings(
+//            array_merge($basicHeader, $optionsHeaders)
+//        );
+//
+//        curl_setopt_array($curl, array(
+//            CURLOPT_URL => $url . $path,
+//            CURLOPT_RETURNTRANSFER => true,
+//            CURLOPT_ENCODING => "",
+//            CURLOPT_MAXREDIRS => 10,
+//            CURLOPT_TIMEOUT => 30,
+//            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+//            CURLOPT_CUSTOMREQUEST => "POST",
+//            CURLOPT_POSTFIELDS => $data_to_send,
+//            CURLOPT_HTTPHEADER => $headers,
+//        ));
 
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => $url . $path,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 30,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "POST",
-            CURLOPT_POSTFIELDS => $data_to_send,
-            CURLOPT_HTTPHEADER => $headers,
-        ));
-
-        $response = curl_exec($curl);
-        $err = curl_error($curl);
-        $http_status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-
-        curl_close($curl);
-
-        if ($err) {
-            return $err;
-        } else {
-            return $response;
-        }
+//        $response = curl_exec($curl);
+//        $err = curl_error($curl);
+//        $http_status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+//
+//        curl_close($curl);
+         return $req;
+//        if ($err) {
+//            return $err;
+//        } else {
+//            return $response;
+//        }
     }
 
     function put($url, $path, $data_to_send, $options = array('headers' => 0, 'transport' => 'https', 'port' => 443, 'proxy' => null))
