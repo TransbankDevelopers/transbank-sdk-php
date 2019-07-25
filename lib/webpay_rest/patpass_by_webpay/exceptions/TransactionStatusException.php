8b38dd4ea9a4cbf5569e2a9b112585838d4202a3
Eugenio Nicolás López Ramos <eugenionlopez@gmail.com>
2019-07-25 11:38:20 -0400

    Add status to Patpass Webpay

---
+++ lib/webpay_rest/patpass_by_webpay/exceptions/TransactionStatusException.php
@@ -0,0 +1,14 @@
+<?php
+namespace Transbank\Webpay\PatPassByWebpay\Exceptions;
+
+use Transbank\Webpay\Exceptions\TransbankException;
+
+class TransactionStatusException extends TransbankException {
+    const DEFAULT_MESSAGE = 'Transaction could not be created. Please verify given parameters';
+
+    public function __construct($message = self::DEFAULT_MESSAGE, $code = 0, $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+
+}
