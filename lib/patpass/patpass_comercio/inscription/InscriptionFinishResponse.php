8dcbc775627f1096646eb99f38c5d803e0fae6db
Nicolas Martinez <nmartinezv@icloud.com>
2019-08-01 11:26:57 -0400

    add inscription finish and status files

---
+++ lib/patpass/patpass_comercio/inscription/InscriptionFinishResponse.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Class InscriptionFinishResponse
+ *
+ * @category
+ * @package Transbank\Patpass\PatpassComercio
+ *
+ */
+
+
+namespace Transbank\Patpass\PatpassComercio;
+
+
+class InscriptionFinishResponse
+{
+
+}
---
+++ lib/patpass/patpass_comercio/inscription/InscriptionStatusResponse.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Class InscriptionStatusResponse
+ *
+ * @category
+ * @package Transbank\Patpass\PatpassComercio
+ *
+ */
+
+
+namespace Transbank\Patpass\PatpassComercio;
+
+class InscriptionStatusResponse
+{
+
+}
