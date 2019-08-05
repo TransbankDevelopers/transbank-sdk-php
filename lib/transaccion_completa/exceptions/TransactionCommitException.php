6cd699693bf8097c03ab15e1a9f8894de070e54f
Nicolas Martinez <nmartinezv@icloud.com>
2019-08-05 15:20:03 -0400

    add transaction commit
    add functions, response and exceptions to commit transaction

--- lib/transaccion_completa/exceptions/TransactionCommitException.php
+++ lib/transaccion_completa/exceptions/TransactionCommitException.php
@@ -12,7 +12,10 @@
 namespace Transbank\TransaccionCompleta\Exceptions;
 
 
-class TransactionCommitException
+class TransactionCommitException extends TransaccionCompletaException
 {
-
+    public function __construct($message = self::DEFAULT_MESSAGE, $code = 0, $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
 }
