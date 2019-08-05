c59d9182cf56708c5e02a904851c6e9a732db445
Nicolas Martinez <nmartinezv@icloud.com>
2019-08-05 16:59:29 -0400

    add refund function

--- lib/transaccion_completa/Transaction.php
+++ lib/transaccion_completa/Transaction.php
@@ -16,13 +16,14 @@ use Transbank\TransaccionCompleta;
 use Transbank\TransaccionCompleta\Exceptions\TransactionCreateException;
 use Transbank\TransaccionCompleta\Exceptions\TransactionInstallmentsException;
 use Transbank\TransaccionCompleta\Exceptions\TransactionCommitException;
+use Transbank\TransaccionCompleta\Exceptions\TransactionRefundException;
 
 class Transaction
 {
     const CREATE_TRANSACTION_ENDPOINT  = '/rswebpaytransaction/api/webpay/v1.0/transactions';
     const INSTALLMENTS_TRANSACTION_ENDPOINT = '/rswebpaytransaction/api/webpay/v1.0/transactions/$TOKEN$/installments';
     const COMMIT_TRANSACTION_ENDPOINT = '/rswebpaytransaction/api/webpay/v1.0/transactions/$TOKEN$';
-    const REFUND_TRANSACTION_ENDPOINT = '';
+    const REFUND_TRANSACTION_ENDPOINT = '/rswebpaytransaction/api/webpay/v1.0/transactions/$TOKEN$/refunds';
     const STATUS_TRANSACTION_ENDPOINT = '';
 
     public function create(
@@ -206,7 +207,62 @@ class Transaction
         $transactionCommitResponse = new TransactionCommitResponse($responseJson);
 
         return $transactionCommitResponse;
+    }
 
+    public function refund(
+        $token,
+        $amount,
+        $options = null
+    ) {
+        if ($options == null) {
+            $commerceCode = TransaccionCompleta::getCommerceCode();
+            $apiKey = TransaccionCompleta::getApiKey();
+            $baseUrl = TransaccionCompleta::getIntegrationTypeUrl();
+        } else {
+            $commerceCode = $options->getCommerceCode();
+            $apiKey = $options->getApiKey();
+            $baseUrl = TransaccionCompleta::getIntegrationTypeUrl($options->getIntegrationType());
+        }
+
+        $headers = [
+            "Tbk-Api-Key-Id" => $commerceCode,
+            "Tbk-Api-Key-Secret" => $apiKey
+        ];
+        $url = str_replace('$TOKEN$', $token, self::COMMIT_TRANSACTION_ENDPOINT);
+
+        $payload = json_encode([
+            "amount" => $amount
+        ]);
+
+        $http = TransaccionCompleta::getHttpClient();
+
+        $httpResponse = $http->post(
+            $baseUrl,
+            $url,
+            $payload,
+            [ 'headers' => $headers ]
+        );
+
+        $httpCode = $httpResponse->getStatusCode();
+
+        if ($httpCode != 200 && $httpCode != 204) {
+            $reason = $httpResponse->getReasonPhrase();
+            $message = "Could not obtain a response from the service: $reason (HTTP code $httpCode)";
+            $body = json_decode($httpResponse->getBody(), true);
+
+            if (isset($body["error_message"])) {
+                $tbkErrorMessage = $body["error_message"];
+                $message = "$message. Details: $tbkErrorMessage";
+            }
+
+            throw new TransactionRefundException($message, $httpCode);
+        }
+
+        $responseJson = json_decode($httpResponse->getBody(), true);
+
+        $transactionCommitResponse = new TransactionRefundResponse($responseJson);
+
+        return $transactionCommitResponse;
 
     }
 
