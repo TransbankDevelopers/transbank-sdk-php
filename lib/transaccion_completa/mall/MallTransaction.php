fe1cfe3619ed787359a495eb54c92b132fd544bd
Nicolas Martinez <nmartinezv@icloud.com>
2019-08-11 12:49:27 -0400

    add refund function

--- lib/transaccion_completa/mall/MallTransaction.php
+++ lib/transaccion_completa/mall/MallTransaction.php
@@ -14,6 +14,7 @@ namespace Transbank\TransaccionCompleta;
 use Transbank\TransaccionCompleta\Exceptions\MallTransactionCommitException;
 use Transbank\TransaccionCompleta\Exceptions\MallTransactionCreateException;
 use Transbank\TransaccionCompleta\Exceptions\MallTransactionInstallmentsException;
+use Transbank\TransaccionCompleta\Exceptions\MallTransactionRefundException;
 use Transbank\TransaccionCompleta\Exceptions\MallTransactionStatusException;
 
 class MallTransaction
@@ -24,6 +25,10 @@ class MallTransaction
     const REFUND_TRANSACTION_ENDPOINT = '/rswebpaytransaction/api/webpay/v1.0/transactions/$TOKEN$/refunds';
     const STATUS_TRANSACTION_ENDPOINT = '/rswebpaytransaction/api/webpay/v1.0/transactions/$TOKEN$';
 
+    /**
+     * @param $commerceCode
+     * @return bool
+     */
     private function validateChild($commerceCode)
     {
         $childlist = MallTransaccionCompleta::getChildCommerceCode();
@@ -63,7 +68,7 @@ class MallTransaction
         ]);
 
         foreach ($details as $detail) {
-            if (!self::validateChild($detail["commerce_code"])) {
+            if (!(new MallTransaction)->validateChild($detail["commerce_code"])) {
                 $message = "Child commerce code is not valid for this parent";
                 $httpCode = 401;
                 throw new MallTransactionCreateException($message, $httpCode);
@@ -186,7 +191,7 @@ class MallTransaction
         $url = str_replace('$TOKEN$', $token, self::COMMIT_TRANSACTION_ENDPOINT);
 
         foreach ($details as $detail) {
-            if (!self::validateChild($detail["commerce_code"])) {
+            if (!(new MallTransaction)->validateChild($detail["commerce_code"])) {
                 $message = "Child commerce code is not valid for this parent";
                 $httpCode = 401;
                 throw new MallTransactionCommitException($message, $httpCode);
@@ -231,6 +236,75 @@ class MallTransaction
         $mallTransactionCommitResponse = new MallTransactionCommitResponse($responseJson);
 
         return $mallTransactionCommitResponse;
+    }
+
+    public static function refund(
+        $token,
+        $buyOrder,
+        $commerceCodeChild,
+        $amount,
+        $options = null
+    )
+    {
+        if ($options == null) {
+            $commerceCode = MallTransaccionCompleta::getCommerceCode();
+            $apiKey = MallTransaccionCompleta::getApiKey();
+            $baseUrl = MallTransaccionCompleta::getIntegrationTypeUrl();
+        } else {
+            $commerceCode = $options->getCommerceCode();
+            $apiKey = $options->getApiKey();
+            $baseUrl = MallTransaccionCompleta::getIntegrationTypeUrl($options->getIntegrationType());
+        }
+
+        $headers = [
+            "Tbk-Api-Key-Id" => $commerceCode,
+            "Tbk-Api-Key-Secret" => $apiKey
+        ];
+        $url = str_replace('$TOKEN$', $token, self::REFUND_TRANSACTION_ENDPOINT);
+
+        if (!(new MallTransaction)->validateChild($commerceCodeChild)) {
+            $message = "Child commerce code is not valid for this parent";
+            $httpCode = 401;
+            throw new MallTransactionStatusException($message, $httpCode);
+        }
+
+        $payload = json_encode([
+            "buy_order" => $buyOrder,
+            "commerce_code" => $commerceCode,
+            "amount" => $amount
+        ]);
+
+        $http = MallTransaccionCompleta::getHttpClient();
+
+        $httpResponse = $http->post(
+            $baseUrl,
+            $url,
+            $payload,
+            [ 'headers' => $headers ]
+        );
+
+        $httpCode = $httpResponse->getStatusCode();
+
+        if ($httpCode != 200 && $httpCode != 204) {
+            $reason = $httpResponse->getReasonPhrase();
+            $message = "Could not obtain a response from the service: $reason (HTTP code $httpCode)";
+            $body = json_decode($httpResponse->getBody(), true);
+
+            if (isset($body["error_message"])) {
+                $tbkErrorMessage = $body["error_message"];
+                $message = "$message. Details: $tbkErrorMessage";
+            }
+
+            throw new MallTransactionRefundException($message, $httpCode);
+        }
+
+        $responseJson = json_decode($httpResponse->getBody(), true);
+
+        $mallTransactionRefundResponse = new MallTransactionRefundResponse($responseJson);
+
+        return $mallTransactionRefundResponse;
+
+
     }
 
     public static function getStatus(
