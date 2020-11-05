1b104b082a3a45432b0068efddfcf0ce9379163c
Gonzalo De Spirito <gonzunigad@gmail.com>
2020-11-05 12:25:40 -0300

    Add support to Guzzle 5.X and 7.X

--- lib/utils/HttpClient.php
+++ lib/utils/HttpClient.php
@@ -1,26 +1,13 @@
 <?php
 namespace Transbank\Utils;
 
+use Guzzle\Http\Exception\RequestException;
 use GuzzleHttp\Client;
 use GuzzleHttp\Psr7\Request;
 
-class HttpClient {
-
-    function post($url, $path, $data_to_send, $options = null) {
-
-        $fullPath = $url . $path;
-        $basicHeader = ["Content-Type" => "application/json"];
-        $givenHeaders = isset($options["headers"]) ? $options["headers"] : [];
-        $headers = array_merge($basicHeader, $givenHeaders);
-
-        $req = new Request('POST', $fullPath,  $headers, $data_to_send);
-        $cl = new Client(['http_errors' => false]);
-
-        $res = $cl->send($req);
-        return $res;
-    }
-
-    function put($url, $path, $data_to_send, $options = null)
+class HttpClient
+{
+    public function get($url, $path, $options = null)
     {
 
         $fullPath = $url . $path;
@@ -28,27 +15,69 @@ class HttpClient {
         $givenHeaders = isset($options["headers"]) ? $options["headers"] : [];
         $headers = array_merge($basicHeader, $givenHeaders);
 
-        $req = new Request('PUT', $fullPath,  $headers, $data_to_send);
+        if (defined('Client::VERSION') && version_compare(Client::VERSION, '6', '<')) {
+            $client = new Client();
+            return $client->get($fullPath, [
+                'headers' => $headers
+            ]);
+        }
+        $req = new Request('GET', $fullPath, $headers);
         $cl = new Client(['http_errors' => false]);
 
-        $res = $cl->send($req);
-        return $res;
+        return $cl->send($req);
     }
 
-    function get($url, $path, $options = null)
+    public function post($url, $path, $data_to_send, $options = null)
+    {
+        $fullPath = $url . $path;
+        $basicHeader = ["Content-Type" => "application/json"];
+        $givenHeaders = isset($options["headers"]) ? $options["headers"] : [];
+        $headers = array_merge($basicHeader, $givenHeaders);
+
+        if (defined('Client::VERSION') && version_compare(Client::VERSION, '6', '<')) {
+            $client = new Client();
+
+            return $client->post($fullPath, [
+                'headers' => $headers,
+                'body' => $data_to_send
+            ]);
+        }
+        $req = new Request('POST', $fullPath, $headers, $data_to_send);
+        $cl = new Client(['http_errors' => false]);
+
+        return $cl->send($req);
+    }
+
+    /**
+     * @param $url
+     * @param $path
+     * @param $data_to_send
+     * @param null $options
+     * @return \GuzzleHttp\Message\FutureResponse|\GuzzleHttp\Message\ResponseInterface|\GuzzleHttp\Ring\Future\FutureInterface|null
+     */
+    public function put($url, $path, $data_to_send, $options = null)
     {
 
         $fullPath = $url . $path;
         $basicHeader = ["Content-Type" => "application/json"];
         $givenHeaders = isset($options["headers"]) ? $options["headers"] : [];
         $headers = array_merge($basicHeader, $givenHeaders);
+        $client = new Client();
 
-        $req = new Request('GET', $fullPath,  $headers);
+        if (defined('Client::VERSION') && version_compare(Client::getGuzzleVersion, '6', '<')) {
+            return $client->put($fullPath, [
+                'headers' => $headers,
+                'body' => $data_to_send
+            ]);
+        }
+
+        $req = new Request('PUT', $fullPath, $headers, $data_to_send);
         $cl = new Client(['http_errors' => false]);
 
-        $res = $cl->send($req);
-        return $res;
+        return $cl->send($req);
     }
+
+
 
     public function delete($url, $path, $data_to_send, $options = null)
     {
@@ -57,10 +86,18 @@ class HttpClient {
         $givenHeaders = isset($options["headers"]) ? $options["headers"] : [];
         $headers = array_merge($basicHeader, $givenHeaders);
 
-        $req = new Request('DELETE', $fullPath,  $headers, $data_to_send);
+        if (defined('Client::VERSION') && version_compare(Client::VERSION, '6', '<')) {
+            $client = new Client();
+
+            return $client->delete($fullPath, [
+                'headers' => $headers,
+                'body' => $data_to_send
+            ]);
+        }
+
+        $req = new Request('DELETE', $fullPath, $headers, $data_to_send);
         $cl = new Client(['http_errors' => false]);
 
-        $res = $cl->send($req);
-        return $res;
+        return $cl->send($req);
     }
 }
