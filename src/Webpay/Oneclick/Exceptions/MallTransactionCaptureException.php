54e0f1df8bc12f22567ad9f5e26f7ed2b17130bd
Felipe Fiebig <felipe.fiebig@continuum.cl>
2020-12-09 16:26:38 -0300

    Remove constructor Oneclick Capture Exception
    Co-authored-by: Gonzalo De Spirito <gonzunigad@gmail.com>

--- src/Webpay/Oneclick/Exceptions/MallTransactionCaptureException.php
+++ src/Webpay/Oneclick/Exceptions/MallTransactionCaptureException.php
@@ -5,8 +5,5 @@ use Transbank\Webpay\Exceptions\WebpayException;
 
 class MallTransactionCaptureException extends WebpayException
 {
-    public function __construct($message = self::DEFAULT_MESSAGE, $code = 0, $previous = null)
-    {
-        parent::__construct($message, $code, $previous);
-    }
+  
 }
