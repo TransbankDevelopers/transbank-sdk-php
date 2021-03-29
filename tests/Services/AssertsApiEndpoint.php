936de75d0c58e4511a184635aebef8e01db641b1
DarkGhosthunter <DarkGhostHunter@gmail.com>
2021-03-29 02:07:25 -0300

    Added helper trait for tests.

--- tests/Services/AssertsApiEndpoint.php
+++ tests/Services/AssertsApiEndpoint.php
@@ -1,4 +1,4 @@
-<?php
+<?php /** @noinspection JsonEncodingApiUsageInspection */
 
 namespace Tests\Services;
 
@@ -7,7 +7,7 @@ use Transbank\Sdk\Http\Connector;
 
 trait AssertsApiEndpoint
 {
-    protected static function assertApiEndpoint(string $endpoint, ServerRequestInterface $request, array $replace = []): void
+    protected static function assertEndpointPath(string $endpoint, ServerRequestInterface $request, array $replace = []): void
     {
         $endpoint = str_replace(
             array_merge(['{api_version}'], array_keys($replace)),
@@ -16,5 +16,21 @@ trait AssertsApiEndpoint
         );
 
         static::assertEquals('/' . $endpoint, $request->getUri()->getPath());
+    }
+
+    protected static function assertRequestContentsEmpty(ServerRequestInterface $request): void
+    {
+        $stream = $request->getBody();
+        $stream->rewind();
+
+        static::assertEmpty($stream->getContents());
+    }
+
+    protected static function assertRequestContents(array $contents, ServerRequestInterface $request): void
+    {
+        $stream = $request->getBody();
+        $stream->rewind();
+
+        static::assertEquals(json_encode($contents), $stream->getContents());
     }
 }
