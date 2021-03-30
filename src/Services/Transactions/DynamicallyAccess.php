c1d26fd2ecdc44765d9d4e9a4044b3b0d96867e2
DarkGhosthunter <DarkGhostHunter@gmail.com>
2021-03-29 21:19:01 -0300

    Fixed snake_case generator.

--- src/Services/Transactions/DynamicallyAccess.php
+++ src/Services/Transactions/DynamicallyAccess.php
@@ -41,13 +41,7 @@ trait DynamicallyAccess
      */
     protected static function toSnakeCase(string $method): string
     {
-        preg_match_all('!([A-Z][A-Z0-9]*(?=$|[A-Z][a-z0-9])|[A-Za-z][a-z0-9]+)!', $method, $matches);
-
-        foreach ($ret = $matches[0] as &$match) {
-            $match = $match === strtoupper($match) ? strtolower($match) : lcfirst($match);
-        }
-
-        return strtolower(implode('_', $ret));
+        return strtolower(ltrim(preg_replace('/[A-Z]([A-Z](?![a-z]))*/', '_$0', $method), '_'));
     }
 
     /**
