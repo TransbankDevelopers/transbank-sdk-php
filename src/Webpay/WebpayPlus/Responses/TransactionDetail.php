f9bb9091bf3f3eaed0e8e9de835892bb58d0b002
DarkFrost <m.astudillo.toledo@gmail.com>
2022-04-20 10:58:25 -0400

    fix approved transaction when response is null

--- src/Webpay/WebpayPlus/Responses/TransactionDetail.php
+++ src/Webpay/WebpayPlus/Responses/TransactionDetail.php
@@ -69,8 +69,20 @@ class TransactionDetail
 
     public function isApproved()
     {
-        return $this->getResponseCode() === ResponseCodesEnum::RESPONSE_CODE_APPROVED &&
-            !in_array($this->getStatus(), [TransactionStatusEnum::STATUS_FAILED]);
+        if($this->getResponseCode() !== ResponseCodesEnum::RESPONSE_CODE_APPROVED) {
+            return false;
+        }
+
+        switch($this->getStatus()) {
+            case TransactionStatusEnum::STATUS_CAPTURED:
+            case TransactionStatusEnum::STATUS_REVERSED:
+            case TransactionStatusEnum::STATUS_NULLIFIED:
+            case TransactionStatusEnum::STATUS_AUTHORIZED:
+            case TransactionStatusEnum::STATUS_PARTIALLY_NULLIFIED:
+                return true;
+            default :
+                return false;
+        }
     }
 
     /**
