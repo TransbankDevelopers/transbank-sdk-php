45e36424c8517cb95957eb7740c5d591550d60ad
Matias Vasquez <m.vasquezac@gmail.com>
2022-09-05 15:32:11 -0400

    Delete comments

--- src/Webpay/WebpayPlus/Responses/TransactionCaptureResponse.php
+++ src/Webpay/WebpayPlus/Responses/TransactionCaptureResponse.php
@@ -11,7 +11,6 @@ class TransactionCaptureResponse
      *
      * @param mixed $responseJson
      */
-    //public $token;
     public $authorizationCode;
     public $authorizationDate;
     public $capturedAmount;
@@ -19,7 +18,6 @@ class TransactionCaptureResponse
 
     public function __construct($json)
     {
-        //$this->token = isset($json['token']) ? $json['token'] : null;
         $this->authorizationCode = isset($json['authorization_code']) ? $json['authorization_code'] : null;
         $this->authorizationDate = isset($json['authorization_date']) ? $json['authorization_date'] : null;
         $this->capturedAmount = isset($json['captured_amount']) ? $json['captured_amount'] : null;
