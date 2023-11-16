91000ae7f4ebdd98a1cdf4f9a3ea09dcd7b18faf
Isabel Anabalón Fuentes <isabel.anabalonfuentes@gmail.com>
2023-11-16 16:14:47 -0300

    refactor: delete oneclick mall deferred capture history response file

--- src/Webpay/Oneclick/Responses/MallDeferredCaptureHistoryResponse.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Transbank\Webpay\Oneclick\Responses;
-
-use Transbank\Common\Responses\BaseDeferredCaptureHistoryResponse;
-
-class MallDeferredCaptureHistoryResponse extends BaseDeferredCaptureHistoryResponse
-{
-}
