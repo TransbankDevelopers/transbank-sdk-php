fd00462ff747e997e4d6ba0377ffbd4386562ca4
Isabel Anabalón Fuentes <isabel.anabalonfuentes@gmail.com>
2023-11-16 15:40:22 -0300

    refactor: delete mall increase authorization date response file

--- src/Webpay/WebpayPlus/Responses/MallIncreaseAuthorizationDateResponse.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Transbank\Webpay\WebpayPlus\Responses;
-
-use Transbank\Common\Responses\BaseDeferredStatusResponse;
-
-class MallIncreaseAuthorizationDateResponse extends BaseDeferredStatusResponse
-{
-}
