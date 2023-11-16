3246f3887d3aec6d0cab334fe888245cfb604f67
Isabel Anabalón Fuentes <isabel.anabalonfuentes@gmail.com>
2023-11-16 15:36:49 -0300

    refactor: delete increase amount response file

--- src/Webpay/WebpayPlus/Responses/IncreaseAmountResponse.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Transbank\Webpay\WebpayPlus\Responses;
-
-use Transbank\Common\Responses\BaseDeferredStatusResponse;
-
-class IncreaseAmountResponse extends BaseDeferredStatusResponse
-{
-}
