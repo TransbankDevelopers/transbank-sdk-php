c3be6d260dd4ca80a84afca4482e028a39962eda
Isabel Anabalón Fuentes <isabel.anabalonfuentes@gmail.com>
2023-11-16 16:16:15 -0300

    refactor: delete oneclick mall reverse preauthorized amount response file

--- src/Webpay/Oneclick/Responses/MallReversePreAuthorizedAmountResponse.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Transbank\Webpay\Oneclick\Responses;
-
-use Transbank\Common\Responses\BaseDeferredStatusResponse;
-
-class MallReversePreAuthorizedAmountResponse extends BaseDeferredStatusResponse
-{
-}
