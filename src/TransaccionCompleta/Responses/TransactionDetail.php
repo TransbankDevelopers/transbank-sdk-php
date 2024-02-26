ebaba4e351a36b03671aea1ceb809bf9e05219dc
victor mendoza <victor.mendoza@continuumhq.com>
2024-02-26 12:48:24 -0300

    refactor: dont use parents createfromArrat instance

--- src/TransaccionCompleta/Responses/TransactionDetail.php
+++ src/TransaccionCompleta/Responses/TransactionDetail.php
@@ -10,10 +10,37 @@ class TransactionDetail extends \Transbank\Webpay\WebpayPlus\Responses\Transacti
 
     public static function createFromArray(array $array)
     {
-        $result = parent::createFromArray($array);
+        $result = new TransactionDetail();
+        $result->amount = Utils::returnValueIfExists($array, 'amount');
+        $result->status = Utils::returnValueIfExists($array, 'status');
+        $result->authorizationCode = Utils::returnValueIfExists($array, 'authorization_code');
+        $result->paymentTypeCode = Utils::returnValueIfExists($array, 'payment_type_code');
+        $result->responseCode = Utils::returnValueIfExists($array, 'response_code');
+        $result->installmentsNumber = Utils::returnValueIfExists($array, 'installments_number');
+        $result->installmentsAmount = Utils::returnValueIfExists($array, 'installments_amount');
+        $result->commerceCode = Utils::returnValueIfExists($array, 'commerce_code');
+        $result->buyOrder = Utils::returnValueIfExists($array, 'buy_order');
+        $result->balance = Utils::returnValueIfExists($array, 'balance');
         $result->prepaidBalance = Utils::returnValueIfExists($array, 'prepaid_balance');
-
 
         return $result;
     }
+
+    /**
+     * @return mixed
+     */
+    public function getPrepaidBalance()
+    {
+        return $this->prepaidBalance;
+    }
+
+    /**
+     * @param mixed $balance
+     */
+    public function setPrepaidBalance($prepaidBalance)
+    {
+        $this->prepaidBalance = $prepaidBalance;
+    }
+
+
 }
