405aabedd8b7f8bb71522e8d40a9a2801a4f182d
Matias Vasquez <m.vasquezac@gmail.com>
2024-04-30 09:17:03 -0400

    test: fix oneclick inscription finish response test

--- tests/Webpay/OneClick/Responses/InscriptionFinishResponseTest.php
+++ tests/Webpay/OneClick/Responses/InscriptionFinishResponseTest.php
@@ -2,21 +2,27 @@
 
 use PHPUnit\Framework\TestCase;
 use Transbank\Webpay\Oneclick\Responses\InscriptionFinishResponse;
-use Transbank\Utils\ResponseCodesEnum;
 
 class OneClickInscriptionFinishResponseTest extends TestCase {
-    /** @test */
-    public function it_can_be_initialized_from_json()
-    {
-        $json = [
+
+    protected $json;
+    protected $inscriptionResponse;
+
+    protected function setUp():void {
+        $this->json = [
             'response_code' => '00',
             'tbk_user' => '123456',
             'authorization_code' => '7890',
             'card_type' => 'Visa',
             'card_number' => '**** **** **** 1234'
         ];
+        $this->inscriptionResponse = new InscriptionFinishResponse($this->json);
 
-        $response = new InscriptionFinishResponse($json);
+    }
+    /** @test */
+    public function it_can_be_initialized_from_json()
+    {
+        $response = new InscriptionFinishResponse($this->json);
 
         $this->assertSame('00', $response->responseCode);
         $this->assertSame('123456', $response->tbkUser);
@@ -28,9 +34,7 @@ class OneClickInscriptionFinishResponseTest extends TestCase {
     /** @test */
     public function it_returns_true_when_response_code_is_approved()
     {
-        $json = ['response_code' => ResponseCodesEnum::RESPONSE_CODE_APPROVED];
-        $response = new InscriptionFinishResponse($json);
-        $this->assertTrue($response->isApproved());
+        $this->assertTrue($this->inscriptionResponse->isApproved());
     }
 
     /** @test */
@@ -42,42 +46,32 @@ class OneClickInscriptionFinishResponseTest extends TestCase {
     }
 
     /** @test */
-    public function it_can_set_and_get_response_code()
+    public function it_can_get_response_code()
     {
-        $response = new InscriptionFinishResponse([]);
-        $response->setResponseCode(200);
-        $this->assertSame(200, $response->getResponseCode());
+        $this->assertSame(0, $this->inscriptionResponse->getResponseCode());
     }
 
     /** @test */
-    public function it_can_set_and_get_tbk_user()
+    public function it_can_get_tbk_user()
     {
-        $response = new InscriptionFinishResponse([]);
-        $response->setTbkUser('123456');
-        $this->assertSame('123456', $response->getTbkUser());
+        $this->assertSame('123456', $this->inscriptionResponse->getTbkUser());
     }
 
     /** @test */
-    public function it_can_set_and_get_authorization_code()
+    public function it_can_get_authorization_code()
     {
-        $response = new InscriptionFinishResponse([]);
-        $response->setAuthorizationCode('7890');
-        $this->assertSame('7890', $response->getAuthorizationCode());
+        $this->assertSame('7890', $this->inscriptionResponse->getAuthorizationCode());
     }
 
     /** @test */
-    public function it_can_set_and_get_card_type()
+    public function it_can_get_card_type()
     {
-        $response = new InscriptionFinishResponse([]);
-        $response->setCardType('Visa');
-        $this->assertSame('Visa', $response->getCardType());
+        $this->assertSame('Visa', $this->inscriptionResponse->getCardType());
     }
 
     /** @test */
-    public function it_can_set_and_get_card_number()
+    public function it_can_get_card_number()
     {
-        $response = new InscriptionFinishResponse([]);
-        $response->setCardNumber('**** **** **** 1234');
-        $this->assertSame('**** **** **** 1234', $response->getCardNumber());
+        $this->assertSame('**** **** **** 1234', $this->inscriptionResponse->getCardNumber());
     }
 }
