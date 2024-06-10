63418a4b9ef5b2196ef762f4850d9f1b907e3806
Matias Vasquez <m.vasquezac@gmail.com>
2024-06-10 17:13:29 -0400

    feat: remove configure methods on patpass comercio

--- src/Patpass/PatpassComercio.php
+++ src/Patpass/PatpassComercio.php
@@ -13,29 +13,4 @@ class PatpassComercio
 {
     const INTEGRATION_API_KEY = 'cxxXQgGD9vrVe4M41FIt';
     const INTEGRATION_COMMERCE_CODE = '28299257';
-
-    protected static $globalOptions = null;
-
-    public static function configureForTesting()
-    {
-        self::configureForIntegration(static::DEFAULT_COMMERCE_CODE, static::DEFAULT_API_KEY);
-    }
-
-    /**
-     * @param $commerceCode
-     * @param string $apiKey
-     */
-    public static function configureForIntegration($commerceCode, $apiKey = Options::DEFAULT_API_KEY)
-    {
-        static::setOptions(Options::forIntegration($commerceCode, $apiKey));
-    }
-
-    /**
-     * @param $commerceCode
-     * @param $apiKey
-     */
-    public static function configureForProduction($commerceCode, $apiKey)
-    {
-        static::setOptions(Options::forProduction($commerceCode, $apiKey));
-    }
 }
