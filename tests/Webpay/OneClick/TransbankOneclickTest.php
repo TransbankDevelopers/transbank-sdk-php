64a82e4324022a3b79dbeb7bbc5d29363cb64f9c
Mauricio Astudillo Toledo <m.astudillo.toledo@gmail.com>
2024-06-12 17:43:15 -0400

    test: add test configure transaction for production

--- tests/Webpay/OneClick/TransbankOneclickTest.php
+++ tests/Webpay/OneClick/TransbankOneclickTest.php
@@ -191,4 +191,19 @@ class TransbankOneclickTest extends TestCase
         $this->assertSame(Options::ENVIRONMENT_INTEGRATION, $inscriptionOptions->getIntegrationType());
         $this->assertSame(Options::BASE_URL_INTEGRATION, $inscriptionOptions->getApiBaseUrl());
     }
+
+    /** @test */
+    public function it_configures_transaction_for_production()
+    {
+        $commerceCode = 'testCommerceCode';
+        $apiKey = 'testApiKey';
+
+        $inscription = MallTransaction::buildForProduction($commerceCode, $apiKey);
+        $inscriptionOptions = $inscription->getOptions();
+
+        $this->assertSame($commerceCode, $inscriptionOptions->getCommerceCode());
+        $this->assertSame($apiKey, $inscriptionOptions->getApiKey());
+        $this->assertSame(Options::ENVIRONMENT_PRODUCTION, $inscriptionOptions->getIntegrationType());
+        $this->assertSame(Options::BASE_URL_PRODUCTION, $inscriptionOptions->getApiBaseUrl());
+    }
 }
