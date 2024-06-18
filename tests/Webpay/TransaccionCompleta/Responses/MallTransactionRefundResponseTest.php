a258711a94245188992c596a05e1bca9addb0ee8
Fernando <rodofernando@gmail.com>
2024-06-18 13:59:35 -0400

    test: update use statements to new path

---
+++ tests/Webpay/TransaccionCompleta/Responses/MallTransactionRefundResponseTest.php
@@ -0,0 +1,55 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Transbank\Webpay\TransaccionCompleta\Responses\MallTransactionRefundResponse;
+
+class MallTransactionRefundResponseTest extends TestCase
+{
+    public function testConstructorAssignsValuesCorrectly()
+    {
+        $json = [
+            'type' => 'testType',
+            'authorization_code' => 'testAuthorizationCode',
+            'authorization_date' => 'testAuthorizationDate',
+            'nullified_amount' => 'testNullifiedAmount',
+            'balance' => 'testBalance',
+            'response_code' => 'testResponseCode',
+        ];
+
+        $response = new MallTransactionRefundResponse($json);
+
+        $this->assertSame('testType', $response->type);
+        $this->assertSame('testAuthorizationCode', $response->authorizationCode);
+        $this->assertSame('testAuthorizationDate', $response->authorizationDate);
+        $this->assertSame('testNullifiedAmount', $response->nullifiedAmount);
+        $this->assertSame('testBalance', $response->balance);
+        $this->assertSame('testResponseCode', $response->responseCode);
+    }
+
+    public function testGetters()
+    {
+        $json = [
+            'type' => 'testType',
+            'authorization_code' => 'testAuthorizationCode',
+            'authorization_date' => 'testAuthorizationDate',
+            'nullified_amount' => 'testNullifiedAmount',
+            'balance' => 'testBalance',
+            'response_code' => 10,
+        ];
+
+        $response = new MallTransactionRefundResponse($json);
+
+        $this->assertSame('testType', $response->getType());
+
+        $this->assertSame('testAuthorizationCode', $response->getAuthorizationCode());
+
+        $this->assertSame('testAuthorizationDate', $response->getAuthorizationDate());
+
+        $this->assertSame('testNullifiedAmount', $response->getNullifiedAmount());
+
+        $this->assertSame('testBalance', $response->getBalance());
+
+        $this->assertSame(10, $response->getResponseCode());
+
+    }
+}
---
+++ tests/Webpay/TransaccionCompleta/Responses/TransactionCreateResponseTest.php
@@ -0,0 +1,30 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Transbank\Webpay\TransaccionCompleta\Responses\TransactionCreateResponse;
+
+class TransactionCompletareateResponseTest extends TestCase
+{
+    /** @test */
+    public function it_can_get_token()
+    {
+        $json = [
+            'token' => 'testToken',
+        ];
+
+        $response = new TransactionCreateResponse($json);
+
+        $this->assertSame('testToken', $response->getToken());
+    }
+
+    /** @test */
+    public function it_returns_null_when_key_does_not_exist_in_json()
+    {
+        $json = [];
+
+        $response = new TransactionCreateResponse($json);
+
+        $this->assertNull($response->getToken());
+    }
+
+}
