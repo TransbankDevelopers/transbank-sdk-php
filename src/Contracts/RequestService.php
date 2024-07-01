54fa7d3f97f61500dfb2cd536b9f95557a9f9eb3
Fernando <rodofernando@gmail.com>
2024-07-01 09:25:26 -0400

    refactor: add type in contracts files

--- src/Contracts/RequestService.php
+++ src/Contracts/RequestService.php
@@ -3,18 +3,19 @@
 namespace Transbank\Contracts;
 
 use Transbank\Webpay\Options;
+use Transbank\Webpay\Exceptions\WebpayRequestException;
 
 interface RequestService
 {
     /**
-     * @param $method
-     * @param $endpoint
-     * @param $payload
+     * @param string  $method
+     * @param string  $endpoint
+     * @param array   $payload
      * @param Options $options
      *
-     * @throws Transbank\Webpay\Exceptions\WebpayRequestException
+     * @throws WebpayRequestException
      *
      * @return array Response from the API as json.
      */
-    public function request($method, $endpoint, $payload, Options $options);
+    public function request(string $method, string $endpoint, array $payload, Options $options);
 }
