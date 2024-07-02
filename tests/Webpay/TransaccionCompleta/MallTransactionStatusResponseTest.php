75c45038910f748103ddf675a00092e77eb639c0
Fernando <rodofernando@gmail.com>
2024-07-02 09:50:43 -0400

    test: fix mall transaction status response test

--- tests/Webpay/TransaccionCompleta/MallTransactionStatusResponseTest.php
+++ tests/Webpay/TransaccionCompleta/MallTransactionStatusResponseTest.php
@@ -9,24 +9,46 @@ class MallTransactionStatusResponseTest extends TestCase
     public function testConstructor()
     {
         $json = [
-            'buyOrder' => '123456',
-            'accounting_date' => '2022-01-01',
+            'buy_order' => '123456',
+            'accounting_date' => '0622',
             'transaction_date' => '2022-01-01 12:00:00',
             'card_detail' => ['card_number' => '1234567890123456'],
             'details' => [
-                ['detail_key1' => 'detail_value1'],
-                ['detail_key2' => 'detail_value2']
+                [
+                    'amount' => 500,
+                    'status' => 'AUTHORIZED',
+                    'authorization_code' => '456456',
+                    'payment_type_code' => 'VN',
+                    'response_code' => 0,
+                    'installments_number' => 1,
+                    'installments_amount' => 500,
+                    'commerce_code' => '597020000540',
+                    'buy_order' => '567890',
+                    'balance' => 0,
+                    'detail_key1' => 'detail_value1',
+                    'detail_key2' => 'detail_value2'
+                ]
             ],
         ];
 
         $response = new MallTransactionStatusResponse($json);
 
-        $this->assertSame($json['buyOrder'], $response->getBuyOrder());
+        $this->assertSame($json['buy_order'], $response->getBuyOrder());
         $this->assertSame($json['accounting_date'], $response->getAccountingDate());
         $this->assertSame($json['transaction_date'], $response->getTransactionDate());
         $this->assertSame($json['card_detail'], $response->getCardDetail());
         $this->assertSame($json['card_detail']['card_number'], $response->getCardNumber());
-        $this->assertCount(2, $response->getDetails());
+        $jsonDetails = $response->getDetails()[0];
+        $this->assertCount(1, $response->getDetails());
+        $this->assertSame(500.00, $jsonDetails->getAmount());
+        $this->assertSame('AUTHORIZED', $jsonDetails->getStatus());
+        $this->assertSame('456456', $jsonDetails->getAuthorizationCode());
+        $this->assertSame('VN', $jsonDetails->getPaymentTypeCode());
+        $this->assertSame(0, $jsonDetails->getResponseCode());
+        $this->assertSame(1, $jsonDetails->getInstallmentsNumber());
+        $this->assertSame(500.00, $jsonDetails->getInstallmentsAmount());
+        $this->assertSame('597020000540', $jsonDetails->getCommerceCode());
+        $this->assertSame('567890', $jsonDetails->getBuyOrder());
     }
 
     public function testReturnValueIfExists()
