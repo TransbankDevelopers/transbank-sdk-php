d4562ac9c3cfb70392b2ec2c995eadca882e6e44
Matias Vasquez <m.vasquezac@gmail.com>
2024-09-09 16:35:13 -0300

    test: add webpay plus transaction details test

---
+++ tests/Webpay/WebpayPlus/Responses/TransactionDetailResponseTest.php
@@ -0,0 +1,26 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Transbank\Webpay\WebpayPlus\Responses\TransactionDetail;
+
+class WebpayDetailResponseTest extends TestCase
+{
+    private TransactionDetail $details;
+
+    public function setUp(): void
+    {
+        $this->details = new TransactionDetail();
+        $this->details->responseCode = -1;
+        $this->details->status = 'AUTHORIZED';
+        $this->details->balance = 100;
+    }
+
+    /** @test */
+    public function it_checks_is_approved()
+    {
+        $this->assertFalse($this->details->isApproved());
+        $this->details->responseCode = 0;
+        $this->assertTrue($this->details->isApproved());
+        $this->details->status = 'FAKE_STATUS';
+        $this->assertFalse($this->details->isApproved());
+    }
