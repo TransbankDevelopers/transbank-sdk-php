e441f3782230ead53c11c34d39721272e1db1651
Matias Vasquez <m.vasquezac@gmail.com>
2024-09-30 17:56:18 -0300

    test: update response test for default protocol value

--- tests/Utils/Curl/ResponseTest.php
+++ tests/Utils/Curl/ResponseTest.php
@@ -20,7 +20,7 @@ class ResponseTest extends TestCase
     /** @test */
     public function it_can_get_class_properties(): void
     {
-        $this->assertEquals('1.1', $this->response->getProtocolVersion());
+        $this->assertEquals('', $this->response->getProtocolVersion());
         $this->assertEquals([
             'Accept' => 'text/plain',
             'api_key' => 'fakeApiKey'
