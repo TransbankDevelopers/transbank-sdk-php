49c834916c3b8e5811a7b0ed5a928ec3eb3b2148
Matias Vasquez <m.vasquezac@gmail.com>
2024-09-30 11:04:14 -0300

    test: set class properties on response

--- tests/Utils/Curl/ResponseTest.php
+++ tests/Utils/Curl/ResponseTest.php
@@ -29,4 +29,34 @@ class ResponseTest extends TestCase
         $this->assertEquals('text/plain', $this->response->getHeaderLine('Accept'));
         $this->assertTrue($this->response->hasHeader('Accept'));
     }
+
+    /** @test */
+    public function it_can_set_class_properties(): void
+    {
+        $newResponse = $this->response->withStatus(404, 'NotFound');
+        $this->assertEquals(404, $newResponse->getStatusCode());
+        $this->assertEquals('NotFound', $newResponse->getReasonPhrase());
+        $this->assertNotSame($this->response, $newResponse);
+
+        $newResponse = $this->response->withProtocolVersion('1.0');
+        $this->assertEquals('1.0', $newResponse->getProtocolVersion());
+        $this->assertNotSame($this->response, $newResponse);
+
+        $newResponse = $this->response->withHeader('testHeader', 'testValue');
+        $this->assertNotSame($newResponse, $this->response);
+
+        $newResponse = $this->response->withAddedHeader('test', 'testValue');
+        $this->assertNotSame($newResponse, $this->response);
+
+        $newResponse = $this->response->withoutHeader('Accept');
+        $this->assertEquals([], $newResponse->getHeader('Accept'));
+        $this->assertNotSame($newResponse, $this->response);
+
+
+        $resource = fopen('php://temp', 'rw+');
+        fwrite($resource, 'testData');
+        $newResponse = $this->response->withBody(new Stream($resource));
+        $this->assertFalse($newResponse->getBody() == $this->response->getBody());
+        $this->assertNotSame($newResponse, $this->response);
+    }
 }
