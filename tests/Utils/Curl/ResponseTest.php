4b7bd048915808e370596f9aa160150de094505a
Matias Vasquez <m.vasquezac@gmail.com>
2024-10-03 12:04:03 -0300

    test: avoid fopen use on response test

--- tests/Utils/Curl/ResponseTest.php
+++ tests/Utils/Curl/ResponseTest.php
@@ -52,10 +52,11 @@ class ResponseTest extends TestCase
         $this->assertEquals([], $newResponse->getHeader('Accept'));
         $this->assertNotSame($newResponse, $this->response);
 
-
-        $resource = fopen('php://temp', 'rw+');
-        fwrite($resource, 'testData');
-        $newResponse = $this->response->withBody(new Stream($resource));
+        $secondResponse = new Response(200, [
+            'Accept' => 'text/plain',
+            'api_key' => 'fakeApiKey'
+        ], 'a text for body');
+        $newResponse = $this->response->withBody($secondResponse->getBody());
         $this->assertFalse($newResponse->getBody() == $this->response->getBody());
         $this->assertNotSame($newResponse, $this->response);
     }
